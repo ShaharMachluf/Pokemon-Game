56aa10d09622f6287e5c166fd5b295e82d9d7c27
ofirrubin <ofirrubin02@gmail.com>
2022-01-03 19:36:53 +0200

    Json Parser

---
+++ GameServerParser.py
@@ -0,0 +1,28 @@
+import json
+
+import networkx as nx
+
+
+class JsonParser:
+    @staticmethod
+    def load_graph(s):
+        p = json.loads(s)
+        g = nx.DiGraph()
+        for n in [JsonParser.__parse_pos(x) for x in p["Nodes"]]:
+            g.add_node(n['id'], pos=n['pos'])
+        for x in p["Edges"]:
+            g.add_edge(x['src'], x['dest'], weight=x['w'])
+        return g
+
+    @staticmethod
+    def __parse_pos(d):  # Dictionary that contains pos as string, converted to tuple.
+        d['pos'] = tuple(d['pos'].split(',')[:2])
+        return d
+
+    @staticmethod
+    def load_agents(s):
+        return [JsonParser.__parse_pos(x["Agent"]) for x in json.loads(s)["Agents"]]
+
+    @staticmethod
+    def get_game_info(s):
+        return json.loads(s)["GameServer"]
