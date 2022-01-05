3f75db1c37623e6e00ec4ba2c808faf74659623f
ShaharMachluf <shahar6261@gmail.com>
2022-01-05 14:31:51 +0200

    finished tests

--- test_algo.py
+++ test_algo.py
@@ -1,22 +1,25 @@
 import unittest
 
 from algo import Ash
-
-
 EPS = 0.0001
 
-class AshTest(unittest.TestCase):
-    def set_up(self):
-        self.a = Ash('127.0.0.1', 6666)
 
-    def test_start_game(self):
-        assert False
+class AshTest(unittest.TestCase):  # test relevent to case 0
 
     def test_find_pokemons(self):
-        assert False
+        a = Ash('127.0.0.1', 6666)
+        exp = [(9, 8)]
+        ans = a.find_pokemons()
+        self.assertEqual(exp, ans)
 
     def test_find_edge(self):
-        assert False
+        a = Ash('127.0.0.1', 6666)
+        exp = (9, 8)
+        pos = a.pokemons[0]["pos"]
+        type_ = a.pokemons[0]["type"]
+        ans = a.find_edge(pos, type_)
+        self.assertEqual(exp, ans)
+
 
     def test_distance(self):
         src = (2, 0)
